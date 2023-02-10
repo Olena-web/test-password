e68d97116095c83f71e7bfb1ba74ed4683e8da32
Olena Honcharuk <gasmin07@gmail.com>
2023-02-09 16:47:30 -0800

    feat: implement check for password

--- src/app/input-password/input_password.component.ts
+++ src/app/input-password/input_password.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {FormsModule} from '@angular/forms';
-import { FormControl } from '@angular/forms';
-/** @title Form field with prefix & suffix */
+// import {MatInputModule} from '@angular/material/input';
+// import {FormsModule} from '@angular/forms';
+// import { FormControl } from '@angular/forms';
+// import { Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-input_password',
@@ -14,6 +15,14 @@ import { FormControl } from '@angular/forms';
 
 export class InputPasswordComponent {
   value = '';
+  letters = /^[A-Za-z]+$/;
+  numbers = /^[0-9]+$/;
+  symbols = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
+  lettersAndNumbers = /^[a-zA-Z0-9]+$/;
+  lettersAndSymbols = /^[a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
+  numbersAndSymbols = /^[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
+  lettersAndNumbersAndSymbols = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
+
   update(value: string) { this.value = value; console.log(this.value, value.length);
     const strong = document.querySelectorAll<HTMLElement>('.strong');
     const first = document.getElementById('1');
@@ -21,43 +30,71 @@ export class InputPasswordComponent {
     const third = document.getElementById('3');
 
     const easy = ()=> {
+      strong.forEach((el) => {
+        el.classList.remove('none');
+        el.classList.remove('red');
+        el.classList.remove('yellow');
+        el.classList.remove('green');
+      })
       first?.classList.add('red');
       second?.classList.add('none');
       third?.classList.add('none');
     }
+
     const medium = ()=> {
+      strong.forEach((el) => {
+        el.classList.remove('none');
+        el.classList.remove('red');
+        el.classList.remove('green');
+      })
       first?.classList.add('yellow');
       second?.classList.add('yellow');
       third?.classList.add('none');
     }
+
     const hard = ()=> {
       strong.forEach((el) => {
+        el.classList.remove('none');
+        el.classList.remove('red');
+        el.classList.remove('yellow');
         el.classList.add('green');
       })
     }
 
 
-    if (value.length === 0) {
+    if (value === '') {
       strong.forEach((el) => {
+        el.classList.remove('red');
+        el.classList.remove('yellow');
         el.classList.add('none');
       }
       );
     }
   else if (value.length <= 8) {
     strong.forEach((el) => {
-       el.classList.add('red');
+      el.classList.remove('none');
+      el.classList.remove('yellow');
+      el.classList.add('red');
   });
 
 }
-else if (value.match(/[a-zA-Z]+/g) || value.match(/[0-9]+/g)) {
+ if (value.length > 8){
+    if ( value.match(this.letters) ) {
   easy();
 }
-else if (value.match(/[^A-Za-z0-9]+/g) || value.match(/[0-9]+/g)) {
+else if (value.match(this.numbers)) {
+  easy();
+}
+else if (value.match(this.symbols)) {
+  easy();
+}
+  else  if (value.match(this.lettersAndNumbers) || value.match(this.lettersAndSymbols) || value.match(this.numbersAndSymbols)) {
   medium();
   }
-else if (value.match(/[a-zA-Z]+/g) && value.match(/[0-9]+/g) && value.match(/[^a-zA-Z\d]+/g)) {
+    else if (value.match(this.lettersAndNumbersAndSymbols)) {
   hard();
   }
+}
 }
   hide = true;
   password: any;
