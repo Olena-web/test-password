d17a496f85949fc89b1962be7a7aa914aa447fa3
Olena Honcharuk <gasmin07@gmail.com>
2023-02-23 15:29:44 -0800

    feat: add ukrainian for regex

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,7 +1,7 @@
-export const LETTERS = /^[A-Za-z]+$/;
+export const LETTERS = /^[A-Za-zа-яієїґ]+$/ig;
   export const NUMBERS = /^[0-9]+$/;
   export const SYMBOLS = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
-  export const LETTERS_NUMBERS = /^[a-zA-Z0-9]+$/;
-  export const LETTERS_SYMBOLS = /^[a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
+  export const LETTERS_NUMBERS = /^[a-zA-ZA-Za-zа-яієїґ0-9]+$/ig;
+  export const LETTERS_SYMBOLS = /^[a-zA-ZA-Za-zа-яієїґ!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
   export const NUMBERS_SYMBOLS = /^[0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
-  export const LETTERS_NUMBERS_SYMBOLS = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/;
+  export const LETTERS_NUMBERS_SYMBOLS = /^[a-zA-ZA-Za-zа-яієїґ0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+$/ig;
