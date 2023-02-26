1a7e33af0a7848b5b7e83b9edc710f81dfcf5481
Olena Honcharuk <gasmin07@gmail.com>
2023-02-26 12:24:56 -0800

    refactor: implement DRY in complexity

--- src/app/complexity-area.ts
+++ src/app/complexity-area.ts
@@ -1,29 +1,20 @@
 import { LETTERS, NUMBERS, SYMBOLS, LETTERS_NUMBERS, LETTERS_SYMBOLS, NUMBERS_SYMBOLS, LETTERS_NUMBERS_SYMBOLS } from './constants';
+import { removeColor } from './helpers';
 
 export const update = function (value:string)  {
+
   const strong = document.querySelectorAll<HTMLElement>('.strong');
   const first = document.getElementById('1');
   const second = document.getElementById('2');
   const third = document.getElementById('3');
 
   const easy = ()=> {
-    strong.forEach((el) => {
-      el.classList.remove('none');
-      el.classList.remove('red');
-      el.classList.remove('yellow');
-      el.classList.remove('green');
-    })
     first?.classList.add('red');
     second?.classList.add('none');
     third?.classList.add('none');
   }
 
   const medium = ()=> {
-    strong.forEach((el) => {
-      el.classList.remove('none');
-      el.classList.remove('red');
-      el.classList.remove('green');
-    })
     first?.classList.add('yellow');
     second?.classList.add('yellow');
     third?.classList.add('none');
@@ -31,49 +22,38 @@ export const update = function (value:string)  {
 
   const hard = ()=> {
     strong.forEach((el) => {
-      el.classList.remove('none');
-      el.classList.remove('red');
-      el.classList.remove('yellow');
       el.classList.add('green');
     })
   }
 
-
   if (value === '') {
     strong.forEach((el) => {
-      el.classList.remove('red');
-      el.classList.remove('yellow');
-      el.classList.remove('green');
+      removeColor(el);
       el.classList.add('none');
     }
-    );
+  )}
+
+ if (value) {
+    strong.forEach((el) => {
+      removeColor(el);
+    });
+
+    if (value.length <= 8) {
+      strong.forEach((el) => {
+      el.classList.add('red')
+    }
+    )}
+    if (value.length > 8){
+      if ( value.match(LETTERS) || value.match(NUMBERS) || value.match(SYMBOLS)) {
+        easy();
+      }
+      else  if (value.match(LETTERS_NUMBERS) || value.match(LETTERS_SYMBOLS) || value.match(NUMBERS_SYMBOLS)) {
+        medium();
+      }
+      else if (value.match(LETTERS_NUMBERS_SYMBOLS)) {
+        hard();
+      }
+    }
   }
-else if (value.length <= 8) {
-  strong.forEach((el) => {
-    el.classList.remove('none');
-    el.classList.remove('yellow');
-    el.classList.remove('green');
-    el.classList.add('red');
-});
-
-}
-if (value.length > 8){
-  if ( value.match(LETTERS) ) {
-easy();
-}
-else if (value.match(NUMBERS)) {
-easy();
-}
-else if (value.match(SYMBOLS)) {
-easy();
-}
-else  if (value.match(LETTERS_NUMBERS) || value.match(LETTERS_SYMBOLS) || value.match(NUMBERS_SYMBOLS)) {
-medium();
-}
-  else if (value.match(LETTERS_NUMBERS_SYMBOLS)) {
-hard();
-}
-}
-
 }
 export default update;
---
+++ src/app/helpers.ts
@@ -0,0 +1,6 @@
+export const removeColor = (el: HTMLElement) => {
+  el.classList.remove('none');
+  el.classList.remove('red');
+  el.classList.remove('yellow');
+  el.classList.remove('green');
+};
